b32a7af3ac0d19032aa6f3d030ba320a29904dc8
boomerang <boomerang.cube@gmail.com>
2014-03-22 15:12:29 +0900

    テンプレートエンジンの実装中(ゲームページ)

--- js/data/games/2013/bakutenzushi.js
+++ js/data/games/2013/bakutenzushi.js
@@ -1,49 +1,19 @@
 var data = {
-    cards: [
-        {
-            id: '1',
-            name: 'レアカード',
-            level: 10,
-            maxLevel: 20,
-            hasSkill: true,
-            skill: 'それなりのスキルだよ',
-            rarity: 'rare'
-        },
-        {
-            id: '2',
-            name: 'ノーマルカード',
-            level: 10,
-            maxLevel: 10,
-            hasSkill: false,
-            skill: '',
-            rarity: 'normal'
-        },
-        {
-            id: '3',
-            name: 'ノーマルカード2',
-            level: 10,
-            maxLevel: 10,
-            hasSkill: false,
-            skill: '',
-            rarity: 'normal'
-        }
-    ],
-    maxCardCount: 100,
-    totalCardCount: 2,
-    userId: '12345'
+    name: '爆転ずし',
+    id: 'bakutenzushi',
+    release: 2013,
+    genre: 'アクション',
+    player: '2~4人(対戦)',
+    time: '3~5',
+    headline: 'ド迫力の寿司バトル！！',
+    description: '寿司を操作して相手を場外に飛ばすんだ！ 10点とったら勝利だ！ ○ボタンを押すとスピンができる！ 相手を遠くに飛ばせるぞ！ アイテムをとると何かが起こる…?! 効果は自分の目で確かめろ！ ステージから飛び出してもあきらめるな！ ふんばりで上手くいけば復帰出来るぞ！<br/>新感覚のアクションゲーム！ ライバルたちをはじき飛ばせ！！'
 };
 
+// テンプレートが定義されたscript要素を探し、そこからテンプレートオブジェクトを作成
+var template = $.templates("#templateGamePage");
 
+// テンプレートとデータモデルを組み合わせてテキストを生成
+var result = template.render(data);
 
-// テンプレートの登録
-jsviews.templates({
-    cardList: document.getElementById('templateCardList').innerHTML
-});
-
-jsviews.views.helpers({
-    dump: function(value) { return JSON.stringify(value);}
-});
-
-console.log(jsviews.render.cardList(data));
 // 描画
-document.getElementById('result').innerHTML = jsviews.render.cardList(data);
+document.getElementById('result').innerHTML = result;
